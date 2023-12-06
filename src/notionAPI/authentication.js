b32c1347646eb4179de248c58b6d2e34a9a01a82
RandonSRProctor <RandyProctor@Gmail.com>
2023-12-05 20:11:50 -0500

    set up basic pseudocode outline

---
+++ src/notionAPI/authentication.js
@@ -0,0 +1,9 @@
+export function getToken() {
+  // goes through authentication steps
+
+  // import environment variable
+
+  // use api key stored in env variable to create session token
+
+  return; // token
+}
---
+++ src/notionAPI/updateList.js
@@ -0,0 +1,9 @@
+import { getToken } from './authentication';
+
+const token = getToken();
+
+export async function updateList(newInput) {
+  // do work to make api call with token and newInput
+
+  console.log('returned message');
+}
